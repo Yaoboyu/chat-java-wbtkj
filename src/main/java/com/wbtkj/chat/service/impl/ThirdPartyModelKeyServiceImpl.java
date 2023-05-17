d759238bd3d56c08c5d9cd0cfe587f81642217a6
yaoboyu <1712683212@qq.com>
2023-05-17 13:50:20 +0800

    补充service

--- src/main/java/com/wbtkj/chat/service/impl/ThirdPartyModelKeyServiceImpl.java
+++ src/main/java/com/wbtkj/chat/service/impl/ThirdPartyModelKeyServiceImpl.java
@@ -1,14 +1,14 @@
 package com.wbtkj.chat.service.impl;
 
 import com.wbtkj.chat.mapper.ThirdPartyModelKeyMapper;
+import com.wbtkj.chat.pojo.dto.thirdPartyModelKey.OpenAIKeyStatus;
 import com.wbtkj.chat.pojo.model.ThirdPartyModelKey;
+import com.wbtkj.chat.pojo.model.ThirdPartyModelKeyExample;
 import com.wbtkj.chat.service.ThirdPartyModelKeyService;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class ThirdPartyModelKeyServiceImpl implements ThirdPartyModelKeyService {
     @Resource
@@ -18,29 +18,35 @@ public class ThirdPartyModelKeyServiceImpl implements ThirdPartyModelKeyService 
 
     @Override
     public List<ThirdPartyModelKey> getAllKey() {
-
-        return null;
+        ThirdPartyModelKeyExample thirdPartyModelKeyExample = new ThirdPartyModelKeyExample();
+        thirdPartyModelKeyExample.createCriteria();
+        List<ThirdPartyModelKey> keys = thirdPartyModelKeyMapper.selectByExample(thirdPartyModelKeyExample);
+        return keys;
     }
 
     @Override
     public boolean addKey(String key, String model) {
-        Openaikey openaikey = new Openaikey();
-        openaikey.setKey(key);
-
-
-        return null;
+        ThirdPartyModelKey thirdPartyModelKey = new ThirdPartyModelKey();
+        thirdPartyModelKey.setModel(model);
+        thirdPartyModelKey.setKey(key);
+        thirdPartyModelKeyMapper.insert(thirdPartyModelKey);
+        return true;
     }
 
     @Override
     public boolean delKey(long id) {
-
-        return null;
+        thirdPartyModelKeyMapper.deleteByPrimaryKey(id);
+        return true;
     }
 
     @Override
     public boolean changeStatus(String key, int status) {
-
-        return null;
+        ThirdPartyModelKey thirdPartyModelKey = new ThirdPartyModelKey();
+        thirdPartyModelKey.setStatus(status);
+        ThirdPartyModelKeyExample thirdPartyModelKeyExample = new ThirdPartyModelKeyExample();
+        thirdPartyModelKeyExample.createCriteria().andKeyEqualTo(key);
+        thirdPartyModelKeyMapper.updateByExample(thirdPartyModelKey,thirdPartyModelKeyExample);
+        return true;
     }
 
     @Override
@@ -52,4 +58,5 @@ public class ThirdPartyModelKeyServiceImpl implements ThirdPartyModelKeyService 
     public List<String> getEnableGpt4Keys() {
         return null;
     }
+
 }
