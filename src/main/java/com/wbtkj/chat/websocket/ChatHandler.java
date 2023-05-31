e850bd8c428673f0d4452a30a07dc3bb93b92fc8
yaoboyu <1712683212@qq.com>
2023-05-31 17:56:51 +0800

    no message

--- src/main/java/com/wbtkj/chat/websocket/ChatHandler.java
+++ src/main/java/com/wbtkj/chat/websocket/ChatHandler.java
@@ -1,14 +1,11 @@
 package com.wbtkj.chat.websocket;
 
 import cn.hutool.json.JSONUtil;
-import com.wbtkj.chat.constant.GeneralConstant;
 import com.wbtkj.chat.constant.RedisKeyConstant;
 import com.wbtkj.chat.exception.MyServiceException;
-import com.wbtkj.chat.filter.OpenAiAuthInterceptor;
 import com.wbtkj.chat.listener.OpenAIWebSocketEventSourceListener;
 import com.wbtkj.chat.mapper.RoleMapper;
 import com.wbtkj.chat.mapper.UserMapper;
-import com.wbtkj.chat.mapper.UserRoleMapper;
 import com.wbtkj.chat.pojo.dto.openai.chat.ChatCompletion;
 import com.wbtkj.chat.pojo.dto.openai.chat.Message;
 import com.wbtkj.chat.pojo.dto.role.WSChatSession;
@@ -17,7 +14,6 @@ import com.wbtkj.chat.pojo.dto.user.UserLocalDTO;
 import com.wbtkj.chat.pojo.model.ChatSession;
 import com.wbtkj.chat.pojo.model.Role;
 import com.wbtkj.chat.pojo.model.User;
-import com.wbtkj.chat.pojo.model.UserRole;
 import com.wbtkj.chat.pojo.vo.role.WSChatMessage;
 import com.wbtkj.chat.service.OpenAiStreamService;
 import com.wbtkj.chat.service.RoleService;
@@ -26,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -34,6 +29,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -41,8 +37,6 @@ import java.util.List;
 @Component
 @Slf4j
 public class ChatHandler extends TextWebSocketHandler {
-
-
     @Resource
     private OpenAiStreamService openAiStreamService;
     @Resource
@@ -53,10 +47,6 @@ public class ChatHandler extends TextWebSocketHandler {
     private RoleMapper roleMapper;
     @Resource
     private UserMapper userMapper;
-    @Resource
-    private UserRoleMapper userRoleMapper;
-    @Resource
-    private OpenAiAuthInterceptor openAiAuthInterceptor;
 //    @Resource
 //    private MongoTemplate mongoTemplate;
     @Resource
@@ -69,7 +59,6 @@ public class ChatHandler extends TextWebSocketHandler {
      * @throws Exception
      */
     @Override
-    @Transactional
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         try {
             String token = session.getHandshakeHeaders().get("authorization").get(0);
@@ -103,9 +92,7 @@ public class ChatHandler extends TextWebSocketHandler {
      * @throws Exception
      */
     @Override
-    @Transactional
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        // TODO 修改hot
         // 取wsChatSession
         String key = RedisKeyConstant.ws_chat_session.getKey() + session.getId();
         WSChatSession wsChatSession = (WSChatSession) redisTemplate.opsForValue().get(key);
@@ -117,33 +104,20 @@ public class ChatHandler extends TextWebSocketHandler {
         }
 
         if (wsChatMessage.getRoleId() == null) {
-            session.sendMessage(new TextMessage("{{wbtkj_error}}:" + "缺少roleId"));
-            return;
-        }
-
-        if (!roleService.checkUserRole(wsChatMessage.getRoleId(), wsChatSession.getUserId())) {
-            session.sendMessage(new TextMessage("{{wbtkj_error}}:" + "未拥有该角色"));
-            return;
+            throw new MyServiceException("缺少roleId");
         }
 
         Role role = roleMapper.selectByPrimaryKey(wsChatMessage.getRoleId());
 
-        if (!openAiAuthInterceptor.hasKey(role.getModel())) {
-            session.sendMessage(new TextMessage("{{wbtkj_error}}:" + role.getModel() + "暂不可用"));
-            return;
-        }
-
         // 扣除用户余额
-        int point = ThirdPartyModelKeyValue.getValue(role.getModel());
-        int newBalance = userService.deductBalance(wsChatSession.getUserId(), point);
-        session.sendMessage(new TextMessage("{{wbtkj_newBalance}}:" + newBalance));
-        // 返现
-        if (role.getUserId() != 0 && !role.getUserId().equals(wsChatSession.getUserId())) { //不是官方角色并且不是角色主人
-            userService.cashBack(role.getUserId(), point);
+        User user = userMapper.selectByPrimaryKey(wsChatSession.getUserId());
+        int value = ThirdPartyModelKeyValue.getValue(role.getModel());
+        if (user.getBalance() - value < 0) {
+            throw new MyServiceException("余额不足");
         }
-        // 增加UserRoleUsed
-        roleService.addUserRoleUsed(role.getId(), wsChatSession.getUserId(), point);
-
+        user.setBalance(user.getBalance() - value);
+        session.sendMessage(new TextMessage("{{userBalance}}:" + user.getBalance()));
+        userMapper.updateByPrimaryKey(user);
 
         // 更新wsChatSession
         if (wsChatSession.getRole() == null
@@ -155,15 +129,10 @@ public class ChatHandler extends TextWebSocketHandler {
                 ChatSession newChatSession = roleService.addChatSession(wsChatSession.getUserId(), role.getId());
                 wsChatSession.setChatSessionId(newChatSession.getChatSessionId());
                 wsChatSession.setMessageList(new ArrayList<>());
-                session.sendMessage(new TextMessage("{{wbtkj_chatSessionId}}:" + newChatSession.getChatSessionId()));
+                session.sendMessage(new TextMessage("{{chatSessionId}}:" + newChatSession.getChatSessionId()));
 
             } else { // 加载历史对话
-                // TODO 鉴权
                 ChatSession chatSession = roleService.getChatSessionById(wsChatMessage.getChatSessionId());
-                if (chatSession == null) {
-                    session.sendMessage(new TextMessage("{{wbtkj_error}}:" + "chatSessionId错误"));
-                    return;
-                }
                 wsChatSession.setChatSessionId(wsChatMessage.getChatSessionId());
                 wsChatSession.setMessageList(chatSession.getMessages());
             }
@@ -173,7 +142,7 @@ public class ChatHandler extends TextWebSocketHandler {
                 ChatSession newChatSession = roleService.addChatSession(wsChatSession.getUserId(), wsChatSession.getRole().getId());
                 wsChatSession.setChatSessionId(newChatSession.getChatSessionId());
                 wsChatSession.setMessageList(new ArrayList<>());
-                session.sendMessage(new TextMessage("{{wbtkj_chatSessionId}}:" + newChatSession.getChatSessionId()));
+                session.sendMessage(new TextMessage("{{chatSessionId}}:" + newChatSession.getChatSessionId()));
 
             } else if (!wsChatMessage.getChatSessionId().equals(wsChatSession.getChatSessionId())) { // 加载历史对话
                 roleService.updateChatSession(wsChatSession.getChatSessionId(), wsChatSession.getMessageList());
@@ -182,11 +151,10 @@ public class ChatHandler extends TextWebSocketHandler {
                 wsChatSession.setMessageList(chatSession.getMessages());
             }
         }
-        List<Message> messages = new ArrayList<>(wsChatSession.getMessageList());
-
         wsChatSession.getMessageList().add(Message.builder().content(wsChatMessage.getMessage()).role(Message.Role.USER).build());
         redisTemplate.opsForValue().set(key, wsChatSession);
 
+        List<Message> messages = new ArrayList<>(wsChatSession.getMessageList());
         messages = messages.subList(Math.max(0, messages.size() - role.getContextN()*2), messages.size());
         messages.add(0, Message.builder().content(role.getSystem()).role(Message.Role.SYSTEM).build());
         messages.add(Message.builder().content(wsChatMessage.getMessage()).role(Message.Role.USER).build());
@@ -217,7 +185,6 @@ public class ChatHandler extends TextWebSocketHandler {
      * @throws Exception
      */
     @Override
-    @Transactional
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         // 用户退出，移除缓存
         WsSessionManager.remove(session.getId());
